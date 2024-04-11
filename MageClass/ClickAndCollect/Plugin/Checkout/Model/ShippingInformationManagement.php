a55f2eaf2b88ae5785fae3d39cf217f4a9bc5fc3
DESKTOP-1NGCE96\sir <sushildesai_99@ymail.com>
2024-04-11 11:37:46 +0530

    Click and Collect module

---
+++ MageClass/ClickAndCollect/Plugin/Checkout/Model/ShippingInformationManagement.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * MageClass_ClickAndCollect Magento Extension
+ *
+ * @category    MageClass
+ * @package     MageClass_ClickAndCollect
+ * @author      Milan Stojanov <milan.stojanov@outlook.com>
+ * @website    http://www.mageclass.com
+ */
+
+namespace MageClass\ClickAndCollect\Plugin\Checkout\Model;
+
+class ShippingInformationManagement
+{
+    protected $quoteRepository;
+
+    public function __construct(
+        \Magento\Quote\Model\QuoteRepository $quoteRepository
+    ) {
+        $this->quoteRepository = $quoteRepository;
+    }
+
+    /**
+     * @param \Magento\Checkout\Model\ShippingInformationManagement $subject
+     * @param $cartId
+     * @param \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+     */
+    public function beforeSaveAddressInformation(
+        \Magento\Checkout\Model\ShippingInformationManagement $subject,
+        $cartId,
+        \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+    ) {
+       
+        $extAttributes = $addressInformation->getExtensionAttributes();
+        //print_r($extAttributes);
+        $pickupDate = $extAttributes->getPickupDate();
+        $pickupStore = $extAttributes->getPickupStore();
+        $quote = $this->quoteRepository->getActive($cartId);
+        $quote->setPickupDate($pickupDate);
+        $quote->setPickupStore($pickupStore);
+        $this->quoteRepository->save($quote);
+        
+    }
+}
